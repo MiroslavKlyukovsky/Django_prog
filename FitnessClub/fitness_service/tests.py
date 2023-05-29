09703737e2007a0cfbf22eaa1aa2a0f742f7f3a7
Miroslav <klyukovskymiroslav@gmail.com>
2023-05-29 12:06:39 +0300

    University lab changed

--- FitnessClub/fitness_service/tests.py
+++ FitnessClub/fitness_service/tests.py
@@ -1,39 +1,3 @@
-# from django.test import TestCase, Client
-# from fitness_service.models import Club, Day, Abonement
-# from django.urls import reverse
-#
-#
-# CLUB_URL = reverse("fitness_service:club-list")
-#
-# class PublicClubTests(TestCase):
-#     def setUp(self) -> None:
-#         self.client = Client()
-#
-#     def test_retrieve_clubs(self):
-#         Club.objects.create(location="somee location")
-#         Club.objects.create(location="some location")
-#
-#         response = self.client.get(CLUB_URL)
-#         clubs = Club.objects.all()
-#
-#         self.assertEqual(response.status_code, 200)
-#         self.assertEqual(list(response.context["club_list"]), list(clubs))
-#         self.assertTemplateUsed(response, "fitness_service/club_list.html")
-#
-# class PublicAbonementTests(TestCase):
-#     def setUp(self) -> None:
-#         self.client = Client()
-#
-#     def test_create_abonement(self):
-#         club = Club.objects.create(location="some location")
-#         day = Day.objects.create(name="Friday", exercise="Box")
-#         form_data = {"club": club, "day": day}
-#
-#         response = self.client.post(reverse("fitness_service:abonement-create", kwargs={"pk": club.pk}), data=form_data)
-#
-#         print(Abonement.objects.first())
-#         self.assertNotEqual(response.status_code, 200)
-#         self.assertFalse(Abonement.objects.filter(club=club).exists())
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth import get_user_model
